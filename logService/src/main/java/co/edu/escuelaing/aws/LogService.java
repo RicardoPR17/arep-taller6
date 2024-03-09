221c7870fb5f1c6b0b3f910e424c960c43905df8
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2024-03-08 21:35:59 -0500

    Cambios manejo mongo

--- logService/src/main/java/co/edu/escuelaing/aws/LogService.java
+++ logService/src/main/java/co/edu/escuelaing/aws/LogService.java
@@ -1,22 +1,23 @@
 package co.edu.escuelaing.aws;
 
+import static com.mongodb.client.model.Sorts.descending;
 import static spark.Spark.get;
 import static spark.Spark.port;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-
-import com.google.gson.Gson;
+import java.util.Map;
 
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
+import com.google.gson.Gson;
+import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Projections;
-
-import static com.mongodb.client.model.Sorts.*;
 
 public class LogService {
     public static void main(String... args) {
@@ -35,23 +36,37 @@ public class LogService {
      * @return A string representation of the JSON with the 10 most recent documents
      */
     private static String getRecords(String newMsg) {
-        MongoDatabase database = MongoUtil.getDB();
-        MongoCollection<Document> messages = database.getCollection("logs");
+        System.out.println(newMsg);
+        try (MongoClient client = new MongoClient("localhost", 27017)) {
+            MongoDatabase database = client.getDatabase("taller6");
+            MongoCollection<Document> messages = database.getCollection("logs");
 
-        // Save the new document in the database
-        Document newDoc = new Document()
-                .append("message", newMsg)
-                .append("date", new Date());
-        messages.insertOne(newDoc);
+            // Save the new document in the database
+            Document newDoc = new Document("message", newMsg)
+                    .append("date", new Date());
+            messages.insertOne(newDoc);
 
-        // Get the 10 most recent documents
-        List<Document> results = new ArrayList<>();
-        Bson projection = Projections.fields(Projections.include("date", "message"), Projections.excludeId());
-        messages.find().sort(descending("date")).projection(projection).limit(10).into(results);
+            // Get the 10 most recent documents
+            List<Document> query = new ArrayList<>();
+            Bson projection = Projections.fields(Projections.include("date", "message"), Projections.excludeId());
+            messages.find().sort(descending("date")).projection(projection).limit(10).into(query);
 
-        // Prepare Gson response
-        Gson json = new Gson();
-        return json.toJson(results);
+            Map<String, Map<String, Object>> result = new HashMap<>();
+            int cont = 1;
+            for (Document doc : query) {
+                Map<String, Object> record = new HashMap<>();
+                record.put("message", doc.getString("message"));
+                record.put("date", ((Date) doc.get("date")).toInstant().toString());
+                result.put(String.valueOf(cont), record);
+                cont++;
+            }
+
+            // Prepare Gson response
+            Gson json = new Gson();
+            return json.toJson(result);
+        } catch (Exception e) {
+            return "Error al registrar el documento en MongoDB";
+        }
     }
 
     /**
