adcd3fcd662ab010737dbe984b8985accd954993
RicardoPR17 <rpulidor17.rpr18@gmail.com>
2024-03-08 19:29:19 -0500

    Adición de funcionalidades con Mongo

--- src/main/java/co/edu/escuelaing/aws/LogService.java
+++ src/main/java/co/edu/escuelaing/aws/LogService.java
@@ -3,10 +3,55 @@ package co.edu.escuelaing.aws;
 import static spark.Spark.get;
 import static spark.Spark.port;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Projections;
+
+import static com.mongodb.client.model.Sorts.*;
+
 public class LogService {
     public static void main(String... args) {
         port(getPort());
-        get("/logfacade", (req, res) -> "{\"msg\": \"test\", \"date\": \"20-02-2024\"}");
+        get("/logfacade", (req, res) -> {
+            String msg = req.queryParams("message");
+            return getRecords(msg);
+        });
+    }
+
+    /**
+     * Save the new document in the database and return the 10 most recent documents
+     * in the collection
+     * 
+     * @param newMsg The message to save in the database
+     * @return A string representation of the JSON with the 10 most recent documents
+     */
+    private static String getRecords(String newMsg) {
+        MongoDatabase database = MongoUtil.getDB();
+        MongoCollection<Document> messages = database.getCollection("logs");
+
+        // Save the new document in the database
+        Document newDoc = new Document()
+                .append("message", newMsg)
+                .append("date", new Date());
+        messages.insertOne(newDoc);
+
+        // Get the 10 most recent documents
+        List<Document> results = new ArrayList<>();
+        Bson projection = Projections.fields(Projections.include("date", "message"), Projections.excludeId());
+        messages.find().sort(descending("date")).projection(projection).limit(10).into(results);
+
+        // Prepare Gson response
+        Gson json = new Gson();
+        return json.toJson(results);
     }
 
     /**
--- src/main/java/co/edu/escuelaing/aws/MongoUtil.java
+++ src/main/java/co/edu/escuelaing/aws/MongoUtil.java
@@ -1,5 +1,15 @@
 package co.edu.escuelaing.aws;
 
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
 public class MongoUtil {
-    private static final String CONNECTION_STRING = "";
+    private static final String CONNECTION_STRING = "mongodb://localhost:27017";
+    private static final String DATABASE_NAME = "taller6";
+
+    public static MongoDatabase getDB() {
+        MongoClient client = MongoClients.create(CONNECTION_STRING);
+        return client.getDatabase(DATABASE_NAME);
+    }
 }
